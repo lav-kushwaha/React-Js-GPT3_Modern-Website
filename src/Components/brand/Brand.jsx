fa991a9b1df97cec620d4a75f57057be171ccd42
Lav Kushwaha <lavkumar1224@gmail.com>
2023-10-06 02:27:24 +0530

    Creating Components

---
+++ src/Components/brand/Brand.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './brand.css'
+
+const Brand = () => {
+  return (
+    <div>brand</div>
+  )
+}
+
+export default Brand
---
+++ src/Containers/blog/Blog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './blog.css'
+
+const Blog = () => {
+  return (
+    <div>blog</div>
+  )
+}
+export default Blog;
