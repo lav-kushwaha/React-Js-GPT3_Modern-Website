6893c3c4a6f83beacc8f3d70a3f502e8b3000419
Lav Kushwaha <lavkumar1224@gmail.com>
2023-10-07 22:02:45 +0530

    Adding Hero Features Brand

--- src/Containers/header/Header.jsx
+++ src/Containers/header/Header.jsx
@@ -19,7 +19,6 @@ const Header = () => (
         <p>1,600 people requested access a visit in last 24 hours</p>
       </div>
     </div>
-
     <div className="gpt3__header-image">
       <img src={ai} />
     </div>
