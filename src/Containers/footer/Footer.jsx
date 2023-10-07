77ebce720f0638ef2b603c504c645c5a67691e0c
Lav Kushwaha <lavkumar1224@gmail.com>
2023-10-07 22:19:56 +0530

    Added Footer

--- src/Containers/footer/Footer.jsx
+++ src/Containers/footer/Footer.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import './footer.css'
+import React from 'react';
+import gpt3Logo from '../../logo.svg';
+import './footer.css';
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
 
-export default Footer
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>Lav Kushwaha, <br /> All Rights Reserved</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>lavkushwahaa@gmail.com</p>
+        <p>8140******</p>
+        <p>Lav Kushwaha</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2023 GPT-3 By Lav Kushwaha. All rights reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
